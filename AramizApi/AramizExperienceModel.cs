2d5371a4d2c1aa05827f8d2d24a1fceb29d39ef8
Emil <emil@aramiz.net>
2019-08-10 17:00:39 +0200

    Created ExpereniceApiModel

---
+++ AramizApi/AramizExperienceModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Text;
+
+namespace Aramiz
+{
+    public class ExperienceModel
+    {
+        public string expId { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime expCreatedDate { get; set; } = DateTime.Now;
+        public DateTime? expModifiyDate { get; set; }
+        public string expCompanyLogo { get; set; }
+        public string expEmployeeTitle { get; set; }
+        public string expCompany { get; set; }
+        public string expLocation { get; set; }
+        public string expCurrentWorkRole { get; set; }
+        public DateTime expStartDate { get; set; }
+        public DateTime expEndDate { get; set; }
+        public string expWorkSubject { get; set; }
+        public string expWorkDescription { get; set; }
+        public string[] expWorkLinks { get; set; }
+    }
+
+    public class ExperienceCreateModel
+    {
+        public string expEmployeeTitle { get; set; }
+        public string expCompanyLogo { get; set; }
+        public string expCompany { get; set; }
+        public string expLocation { get; set; }
+        public string expCurrentWorkRole { get; set; }
+        public DateTime expStartDate { get; set; }
+        public DateTime expEndDate { get; set; }
+        public string expWorkSubject { get; set; }
+        public string expWorkDescription { get; set; }
+        public string[] expWorkLinks { get; set; }
+    }
+
+    public class ExperienceUpdateModel
+    {
+        public DateTime expModifiyDate { get; set; } = DateTime.Now;
+        public string expEmployeeTitle { get; set; }
+        public string expCompanyLogo { get; set; }
+        public string expCompany { get; set; }
+        public string expLocation { get; set; }
+        public string expCurrentWorkRole { get; set; }
+        public DateTime expStartDate { get; set; }
+        public DateTime expEndDate { get; set; }
+        public string expWorkSubject { get; set; }
+        public string expWorkDescription { get; set; }
+        public string[] expWorkLinks { get; set; }
+    }
+
+    public class ExperienceEntity : TableEntity
+    {
+        public DateTime expCreatedDate { get; set; }
+        public DateTime? expModifiyDate { get; set; }
+        public string expCompanyLogo { get; set; }
+        public string expEmployeeTitle { get; set; }
+        public string expCompany { get; set; }
+        public string expLocation { get; set; }
+        public string expCurrentWorkRole { get; set; }
+        public DateTime expStartDate { get; set; }
+        public DateTime expEndDate { get; set; }
+        public string expWorkSubject { get; set; }
+        public string expWorkDescription { get; set; }
+        public string[] expWorkLinks { get; set; }
+    }
+
+    public static class ExpericenceMappings
+    {
+        public static ExperienceEntity ExperienceTableEntity(this ExperienceModel experience)
+        {
+            return new ExperienceEntity
+            {
+                PartitionKey = "experience",
+                RowKey = experience.expId,
+                expCreatedDate = experience.expCreatedDate,
+                expCompanyLogo = experience.expCompanyLogo,
+                expEmployeeTitle = experience.expEmployeeTitle,
+                expCompany = experience.expCompany,
+                expLocation = experience.expLocation,
+                expCurrentWorkRole = experience.expCurrentWorkRole,
+                expStartDate = experience.expStartDate,
+                expEndDate = experience.expEndDate,
+                expWorkSubject = experience.expWorkSubject,
+                expWorkDescription = experience.expWorkDescription,
+                expWorkLinks = experience.expWorkLinks
+            };
+        }
+
+        public static ExperienceModel ExperienceEntity(this ExperienceEntity experience)
+        {
+            return new ExperienceModel()
+            {
+                expId = experience.RowKey,
+                expCreatedDate = experience.expCreatedDate,
+                expModifiyDate = experience.expModifiyDate,
+                expCompanyLogo = experience.expCompanyLogo,
+                expEmployeeTitle = experience.expEmployeeTitle,
+                expCompany = experience.expCompany,
+                expLocation = experience.expLocation,
+                expCurrentWorkRole = experience.expCurrentWorkRole,
+                expStartDate = experience.expStartDate,
+                expEndDate = experience.expEndDate,
+                expWorkSubject = experience.expWorkSubject,
+                expWorkDescription = experience.expWorkDescription,
+                expWorkLinks = experience.expWorkLinks
+            };
+        }
+    }
+}
